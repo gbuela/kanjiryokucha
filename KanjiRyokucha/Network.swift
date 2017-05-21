994437b2b09513b7b54a9aa00bada8e85d05b609
German Buela <gbuela+gh@gmail.com>
2017-05-21 16:38:22 -0300

    fixes #43

--- KanjiRyokucha/Network.swift
+++ KanjiRyokucha/Network.swift
@@ -194,7 +194,7 @@ extension Request {
     func joinParams(_ paramsDict: ParamSet) -> String {
         let params = paramsDict.map { (key: String, value: String) -> String in
             if let encodedKey = key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-                let encodedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                let encodedValue = value.addingPercentEncodingForURLQueryValue() {
                 return encodedKey + "=" + encodedValue
             } else {
                 return ""
@@ -221,3 +221,44 @@ extension Request {
     }
 }
 
+
+/*
+ From http://stackoverflow.com/a/35912606/17138
+ This solves escaping issues, eg with passwords containing characters like &
+ */
+
+extension String {
+    
+    /// Returns a new string made from the `String` by replacing all characters not in the unreserved
+    /// character set (as defined by RFC3986) with percent encoded characters.
+    
+    func addingPercentEncodingForURLQueryValue() -> String? {
+        let allowedCharacters = CharacterSet.urlQueryValueAllowed()
+        return addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    }
+    
+}
+
+extension CharacterSet {
+    
+    /// Returns the character set for characters allowed in the individual parameters within a query URL component.
+    ///
+    /// The query component of a URL is the component immediately following a question mark (?).
+    /// For example, in the URL `http://www.example.com/index.php?key1=value1#jumpLink`, the query
+    /// component is `key1=value1`. The individual parameters of that query would be the key `key1`
+    /// and its associated value `value1`.
+    ///
+    /// According to RFC 3986, the set of unreserved characters includes
+    ///
+    /// `ALPHA / DIGIT / "-" / "." / "_" / "~"`
+    ///
+    /// In section 3.4 of the RFC, it further recommends adding `/` and `?` to the list of unescaped characters
+    /// for the sake of compatibility with some erroneous implementations, so this routine also allows those
+    /// to pass unescaped.
+    
+    
+    static func urlQueryValueAllowed() -> CharacterSet {
+        return CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~/?")
+    }
+    
+}
